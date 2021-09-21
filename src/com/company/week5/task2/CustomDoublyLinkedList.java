3cc975586ca88761ecd72805402983a0042f6145
Jane <jeyyuk@gmail.com>
2021-09-21 15:39:25 +0400

    update homework

--- src/com/company/week5/task2/CustomDoublyLinkedList.java
+++ src/com/company/week5/task2/CustomDoublyLinkedList.java
@@ -1,33 +1,43 @@
-package com.company.week5.task2;
-
-public class CustomDoublyLinkedList <T>{
-    private class DoublyLinkedNode <T> {
-        private final T element;
-        private DoublyLinkedNode next;
-        private DoublyLinkedNode prev;
-        private DoublyLinkedNode first;
-        private DoublyLinkedNode last;
-        private DoublyLinkedNode size;
-        private DoublyLinkedNode currentNode;
-
-        public DoublyLinkedNode(T element) {
-            this.element = element;
-        }
-        public void addAtTheBeginning(T element) throws Exception {
-            restrictDuplicate(element);
-
-        }
-        public int search (T element) {
-            for(int i = 0; i < size; i++) {
-                if (element.equals(currentNode)) {
-                    return i;
-                }
-            } return -1;
-        }
-        private void restrictDuplicate(T element) throws Exception {
-            if (search(element) != -1){
-                throw new Exception("Element has already exist.");
-            }
-        }
-    }
-}
+//package com.company.week5.task2;
+//
+//public class CustomDoublyLinkedList <T>{
+//    private class DoublyLinkedNode <T> {
+//        private final T element;
+//        private DoublyLinkedNode<T> next;
+//        private DoublyLinkedNode<T> prev;
+//        private DoublyLinkedNode<T> first;
+//        private DoublyLinkedNode<T> last;
+//        private DoublyLinkedNode currentNode;
+//        private int size;
+//
+//        public DoublyLinkedNode(T element) {
+//            this.element = element;
+//        }
+//        public void addAtTheBeginning(T element) throws Exception {
+//            restrictDuplicate(element);
+//            DoublyLinkedNode<T> doublyLinkedNode = new DoublyLinkedNode<>(element);
+//            if(first == null) {
+//                first = doublyLinkedNode;
+//                last = first;
+//            } else {
+//                first.prev = doublyLinkedNode;
+//                doublyLinkedNode.next = first;
+//                first= doublyLinkedNode;
+//                }
+//            } size++;
+//
+//        }
+//        public int search (T element) {
+//            for(int i = 0; i < size; i++) {
+//                if (element.equals(currentNode)) {
+//                    return i;
+//                }
+//            } return -1;
+//        }
+//        private void restrictDuplicate(T element) throws Exception {
+//            if (search(element) != -1){
+//                throw new Exception("Element has already exist.");
+//            }
+//        }
+//    }
+//}
