9773b3cddea58d8c625ad927f808650a561da95a
Jane <jeyyuk@gmail.com>
2021-09-26 15:30:56 +0400

    update homework

--- src/com/company/week11/task1/LinesInFile.java
+++ src/com/company/week11/task1/LinesInFile.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class LinesInFile implements Runnable{
