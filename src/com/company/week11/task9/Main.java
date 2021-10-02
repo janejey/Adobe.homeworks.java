b8573523d7c3a2d08cd1c7ead6a7a844e8e7ff10
Jane <jeyyuk@gmail.com>
2021-10-02 18:12:39 +0400

    update homework

---
+++ src/com/company/week11/task9/Main.java
@@ -0,0 +1,8 @@
+package com.company.week11.task9;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryFile binaryFile = new BinaryFile();
+        binaryFile.run();
+    }
+}
